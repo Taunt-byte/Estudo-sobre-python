b0e6b6029b5c8edef29e1d4098cfb766a94ef727
Lucas Neves <71947586+Taunt-byte@users.noreply.github.com>
2021-08-27 17:18:41 -0300

    dia 2

--- ex3N2.py
+++ ex3N2.py
@@ -1,9 +1,10 @@
 # Fazer um codigo que diga se o numero é igual ou impar
 
-
-a = int (input('Entre com um valor'))
-resto = a & 2
-if resto == 0:
+a = int (input('Entre com o primeiro valor'))
+b = int(input('Entre com o segundo valor'))
+resto_a = a & 2
+resto_b = b & 2
+if resto_a == 0 or resto_b == 0:
     print('numero é par')
 else:
     print('numero é impar')
