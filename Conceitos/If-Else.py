8226e39d5d5107706936e8df4322d0895b249759
Lucas Neves <71947586+Taunt-byte@users.noreply.github.com>
2022-01-27 15:40:14 -0300

    Update If-Else.py

--- Conceitos/If-Else.py
+++ Conceitos/If-Else.py
@@ -1,5 +1,12 @@
-# Soma de 0 a 99
+# Exemplo 1
 s = 0
 for x in range(1, 100):
  s = s + x
 print ( s )
+
+# Exemplo 2
+
+comida = 'pizza'
+
+if comida=='pizza':
+    print('Possui muitas calorias')
