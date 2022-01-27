1008e19b34db844cf26be054c17df1104889b497
Lucas Neves <71947586+Taunt-byte@users.noreply.github.com>
2022-01-27 12:15:12 -0300

    Update Conceitos

---
+++ Conceitos/format.py
@@ -0,0 +1,7 @@
+# Programa para converter graus de Celsius para Fahrenheit
+
+c = 25 
+f = 1.8*c + 32 
+print('25 graus Celsius = ', f, ' graus Fahrenheit')
+
+# Serve basicamente para imprimir argumentos
