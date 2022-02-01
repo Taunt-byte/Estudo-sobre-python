33fd23a769dfbd9c5ca26a383082b999db6f7d8d
Lucas Neves <71947586+Taunt-byte@users.noreply.github.com>
2022-01-31 23:43:37 -0300

    Update Exercicio_III.py

---
+++ Exercicios/Exercicio_III.py
@@ -0,0 +1,9 @@
+# Somando dois números
+
+# Lembre-se dos tipos primitivos para que o 1 + 1 nâo vire 11
+
+print("Digite dois valores para a soma")
+num1 = int(input("Digite um valor"))
+num2 = int(input("Digite outro valor"))
+soma = num1 + num2
+print("o valor da soma é {}!".format(soma))
